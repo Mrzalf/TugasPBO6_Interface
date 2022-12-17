661bed4dc764930c13c7adcf58d972b10942964c
muhammadrizaldyalifiansyah <rizaldy.alifiansyah27@gmail.com>
2022-12-17 21:21:09 +0700

    Implementasi Interface

---
+++ src/Customer_Priority.java
@@ -0,0 +1,6 @@
+public class Customer_Priority extends Benefit {
+    @Override
+    public String getBenefit() {
+        return "Keuntungan menjadi customer priority adalah mendapatkan prioritas layanan dan harga terbaik untuk penggadaian emas.";
+    }
+}
---
+++ src/Customer_Reguler.java
@@ -0,0 +1,6 @@
+public class Customer_Reguler extends Benefit {
+    @Override
+    public String getBenefit() {
+        return "Keuntungan menjadi customer reguler adalah dapat menggunakan jasa penggadaian emas dengan harga standar.";
+    }
+}
